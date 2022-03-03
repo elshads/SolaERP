c408873e22122ad5ead2cd0ba6147306d420b7a5
arifhasanov@yahoo.com <37578820+arifhasanov@users.noreply.github.com>
2022-03-03 12:53:30 +0100

    theme controller

---
+++ SolaERP/Server/Controllers/ThemeController.cs
@@ -0,0 +1,26 @@
+﻿namespace SolaERP.Server.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class ThemeController : Controller
+{
+    [HttpPut]
+    public async Task<string> UpdateUserTheme([FromBody]AppUser user)
+    {
+        string result = "User theme saved";
+        try
+        {
+            using (var cn = new SqlConnection(SqlConfiguration.StaticConnectionString))
+            {
+                var sql = $"UPDATE AppUser SET Theme = {user.Theme} WHERE Id = {user.Id}";
+                var _result = await cn.ExecuteAsync(sql);
+            }
+        }
+        catch (Exception ex)
+        {
+            result = ex.Message;
+        }
+        return result;
+    }
+}
