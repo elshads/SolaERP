87c01a1dd37c889ed8c5d307dcca90a456a8397e
arifhasanov@yahoo.com <37578820+arifhasanov@users.noreply.github.com>
2022-03-08 23:19:26 +0100

    Groups changed to BaseService

--- SolaERP/Server/Controllers/GroupController.cs
+++ SolaERP/Server/Controllers/GroupController.cs
@@ -15,6 +15,34 @@ public class GroupController : Controller
     [HttpGet]
     public async Task<IEnumerable<Group>> GetAllAsync()
     {
-        return await _groupService.GetAllAsync();
+        var sql = $"dbo.SP_GroupMain_Load";
+        var p = new DynamicParameters();
+
+        var result = await _groupService.GetAllAsync(sql, p, CommandType.StoredProcedure);
+        return result.ResultList;
+    }
+
+    [HttpGet]
+    public async Task<Group> GetByIdAsync([FromBody] Group group)
+    {
+        var sql = $"dbo.SP_GroupHeader_Load";
+        var p = new DynamicParameters();
+        p.Add("@GroupId", group.GroupId, DbType.Int32, ParameterDirection.Input);
+
+        return await _groupService.GetSingleAsync(sql, p, CommandType.StoredProcedure);
+    }
+
+    [HttpDelete]
+    public async Task<SqlResult> DeleteAsync([FromBody] List<int> deleteList)
+    {
+        var sql = $"dbo.SP_Groups_IUD";
+        return await _groupService.DeleteAsync(deleteList, sql, "GroupId");
+    }
+
+    [HttpPost]
+    public async Task<SqlResult> InsertAsync([FromBody] List<Group> groups)
+    {
+        var sql = $"dbo.SP_Groups_IUD";
+        return await _groupService.InsertAsync(groups, sql);
     }
 }
