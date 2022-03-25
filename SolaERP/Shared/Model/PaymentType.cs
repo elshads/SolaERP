3be8901a07b9806155879e9aa88d4373bedcf4f2
Elshad Shabanov <elshad@live.com>
2022-03-26 00:10:07 +0100

    PaymentsCard (WIP)

---
+++ SolaERP/Shared/Model/PaymentType.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SolaERP.Shared.Model
+{
+    public class PaymentType : BaseModel
+    {
+        public int PaymentTypeId { get; set; }
+        public string PaymentTypeName { get; set; }
+    }
+}
