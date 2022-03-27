146eb968ce56eb5c60ba83601b71811084c3a5d7
Elshad Shabanov <elshad@live.com>
2022-03-27 01:58:35 +0100

    DataGrid fixes

--- SolaERP/Shared/Model/PaymentType.cs
+++ SolaERP/Shared/Model/PaymentType.cs
@@ -3,7 +3,9 @@ namespace SolaERP.Shared.Model
 {
     public class PaymentType : BaseModel
     {
-        public int PaymentTypeId { get; set; }
-        public string PaymentTypeName { get; set; }
+        public int PaymentDocumentType { get; private set; }
+        public string PaymentDocumentTypeName { get; private set; }
+
+        public static IEnumerable<PaymentType> PaymentTypeList { get; private set; } = new List<PaymentType> { new PaymentType() { PaymentDocumentType=1, PaymentDocumentTypeName="Order" }, new PaymentType() { PaymentDocumentType = 2, PaymentDocumentTypeName = "Advance" } };
     }
 }
